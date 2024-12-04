7b1ff0456dda2a6170efb2dfa6d7a250d9770baa
Artur <artur.brodovic@ubiops.com>
2024-12-04 14:22:56 +0100

    Removed most of python tests

--- gsp.py
+++
@@ -1,6 +0,0 @@
-from trueskill import Gaussian
-
-g1 = Gaussian(1, 2)
-g2 = Gaussian(2, 3)
-
-sampler = Gaussian(3, 0.5)
--- kldiv.py
+++
@@ -1,6 +0,0 @@
-
-from trueskill.mathematics import Gaussian
-if __name__ == "__main__":
-    gs = Gaussian(mu=2, sigma=3)
-    print(gs)
-
